78228d2e9dbf00c48172c43da6017e7d94b4c6d3
LuanRoger <luan.roger.2003@gmail.com>
2022-06-23 10:12:02 -0300

    Fix create id_generator for BSTEncomenda

--- Generators/id_generator.h
+++ Generators/id_generator.h
@@ -1,6 +1,11 @@
+#include "../Data-structures/bst_encomenda.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 #ifndef ID_GENERATOR_H
 #define ID_GENERATOR_H
 
-int id_generator();
+int id_generator(BSTEncomendas* bst);
 
 #endif
